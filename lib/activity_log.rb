0e05d8c0366fa84fe333cc8ac08dda78ff2c8475
abloke <abloke>
2007-02-02 16:19:11 +0000

    Added :additional option to find_with, allowing more sql terms to be tagged on

--- lib/activity_log.rb
+++ lib/activity_log.rb
@@ -44,7 +44,12 @@ class ActivityLog < ActiveRecord::Base
   def self.find_with(options={})
     limit = (options.delete(:limit) || 10)
     order = options.delete(:order)
+    if options[:additional]
+      additionals = options[:additional].is_a?Array ? options[:additional] : [options[:additional]]
+    end
+    options.delete(:additional)
     conditions = self.build_sql_conditional_for(options)
+    conditions << (conditions ? " AND " : "") << options[:additional].join(" #{operator || "AND"} ") if options[:additional]
     self.find(:all, :conditions => conditions, :limit => limit, :order => order)
   end
 
