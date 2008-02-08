b9a466b60ac9599046b91a34a378c6871d2d82ee
abloke <abloke>
2008-02-08 12:46:12 +0000

    Added new migration code, from Justin

--- generators/activity_logger_migration/templates/migration.rb
+++ generators/activity_logger_migration/templates/migration.rb
@@ -1,15 +1,13 @@
 class <%= class_name %> < ActiveRecord::Migration
   def self.up
     create_table :activity_logs, :options => "DEFAULT CHARSET = utf8" do |t|
-      t.column :user_id, :integer
-      t.column :activity_loggable_type, :string
-      t.column :activity_loggable_id, :integer
-      t.column :action, :string
-      t.column :created_at, :datetime
-      t.column :culprit_id, :integer
-      t.column :culprit_type, :string
-      t.column :referenced_id, :integer
-      t.column :referenced_type, :string
+      # Thanks to 'Justin' for an updated migration script, much cleaner!
+      t.belongs_to :user 
+      t.string :action 
+      t.references :activity, :null => false, :polymorphic => true 
+      t.references :culprit, :null => false, :polymorphic => true 
+      t.references :referenced, :null => false, :polymorphic => true 
+      t.timestamps 
     end
   end
 
