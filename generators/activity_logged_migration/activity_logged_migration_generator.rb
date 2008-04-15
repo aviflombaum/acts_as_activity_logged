01e1de6d0e41a50f1132e1935062237dfd06cb09
abloke <abloke>
2008-04-15 21:48:39 +0000

    fixed a faulty class name, oops

--- generators/activity_logged_migration/activity_logged_migration_generator.rb
+++ generators/activity_logged_migration/activity_logged_migration_generator.rb
@@ -1,4 +1,4 @@
-class AcitivityLoggedMigrationGenerator < Rails::Generator::NamedBase
+class ActivityLoggedMigrationGenerator < Rails::Generator::NamedBase
   def manifest
     record do |m|
       m.migration_template 'migration.rb', 'db/migrate'
